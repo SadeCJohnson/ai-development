67dbc379721d902dfe1f690a9c90cf3041d680ad
Sadé C. Johnson <60587066+SadeCJohnson@users.noreply.github.com>
2023-11-22 17:25:14 -0600

    Added code for the ai youtube assistant.

---
+++ langchain-llm-apps/youtube-assistant/main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import youtube_assistant as yta 
+import textwrap #Gives the ability to wrap text so we don't have to scroll the page
+
+
+st.title("YouTube Assistant")
+
+with st.sidebar:
+    with st.form(key="my_form"):
+        youtube_url = st.sidebar.text_area(
+            label="Enter the url for the YouTube video: ",
+            max_chars=50 #don't want the video url to exceed 50 characters
+        )
+
+        query = st.sidebar.text_area(
+            label="Ask me about the video?",
+            max_chars=50,
+            key="query"
+        )
+
+
+        submit_button = st.form_submit_button(label='Submit')
+
+
+if query and youtube_url: #if both of the paramaters exist
+     db = yta.create_vector_db_from_youtube_url(youtube_url)
+     response, docs = yta.get_response_from_query(db, query) #FIXME: this line throws the following error: ValueError: too many values to unpack (expected 2) 
+     st.subheader("Answer:")
+     st.text(textwrap.fill(response, width = 85))
