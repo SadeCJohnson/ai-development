4e3dda5c371b173f5f5b9dfe78e7374203a36067
Sadé C. Johnson <60587066+SadeCJohnson@users.noreply.github.com>
2023-11-27 22:52:59 -0600

    Added the OpenAI API Key field
    This allows the user to use their own API Key to interact with the Youtube Assistant.

--- langchain-llm-apps/youtube-assistant/main.py
+++ langchain-llm-apps/youtube-assistant/main.py
@@ -7,6 +7,7 @@ st.title("YouTube Assistant")
 
 with st.sidebar:
     with st.form(key="my_form"):
+        submit_button = st.form_submit_button(label='Submit') 
         youtube_url = st.sidebar.text_area(
             label="Enter the url for the YouTube video: ",
             max_chars=50 #don't want the video url to exceed 50 characters
@@ -17,12 +18,20 @@ with st.sidebar:
             max_chars=50,
             key="query"
         )
+        openai_api_key = st.sidebar.text_input(
+            label="Insert your [OpenAI API Key](https://platform.openai.com/api-keys):",
+            key="youtube_assistant_search_openai_apikey",
+            max_chars=60,
+            type="password"
+        )
 
-
-        submit_button = st.form_submit_button(label='Submit')
 
 
 if query and youtube_url: #if both of the paramaters exist
+    if not openai_api_key:
+         st.info("Please insert your OpenAI API key in order to unleash the power of this AI Youtube Assistant.")
+         st.stop()
+    else:
      db = yta.create_vector_db_from_youtube_url(youtube_url)
      response, docs = yta.get_response_from_query(db, query)
      st.subheader("Answer:")
