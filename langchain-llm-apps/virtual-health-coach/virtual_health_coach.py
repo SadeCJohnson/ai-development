cf84bb5dc1aadcdbd408e755ce3cdff8cdc6b28b
Sadé C. Johnson <60587066+SadeCJohnson@users.noreply.github.com>
2024-02-28 16:45:05 -0600

    Updated the gpt model since the previous one didn't support chat completions
    chat completions are required for the AI monitoring capability to be triggered and captured in the New Relic platform.
    
    Also updated some dependencies in the requirements.txt file.

--- langchain-llm-apps/virtual-health-coach/virtual_health_coach.py
+++ langchain-llm-apps/virtual-health-coach/virtual_health_coach.py
@@ -1,23 +1,23 @@
-from langchain.document_loaders import YoutubeLoader 
+from langchain_community.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter 
-from langchain.llms.openai import OpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings 
+from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings 
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.vectorstores.faiss import FAISS # faiss -> Facebook AI Similarity Search library
+from langchain_community.vectorstores import FAISS # faiss -> Facebook AI Similarity Search library
 from dotenv import load_dotenv
 import newrelic.agent
+from langchain.callbacks import StdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
 
-#newrelic.agent.initialize('/newrelic.ini')
 # New Relic AI Monitoring - STEP 1
 from nr_openai_observability import monitor
+from nr_openai_observability.langchain_callback import NewRelicCallbackHandler
 
 load_dotenv()
 
 # Initialize New Relic CallBack Handler, for the AI Monitoring - STEP 2
-
-#Create model monitor
-monitor.initialization()
+new_relic_monitor = NewRelicCallbackHandler()
 
 embeddings = OpenAIEmbeddings()
 
@@ -30,7 +30,7 @@ def create_vector_db_from_youtube_url(video_url:str) -> FAISS:
     transcript = loader.load()  #saves the video into the transcript variable
 
     #because there is a token limit on how much info we can send to openai, we split the amount of context we send for a youtube transcript
-    #this demo uses the GPT3.5 model, specifically the gpt-3.5-turbo-instruct, which has a context window of 4096 tokens
+    #this demo uses the GPT3.5 model, specifically the gpt-3.5-turbo, which has a context window of 4096 tokens
     #so we're splitting it and storing it into vector stores
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     docs = text_splitter.split_documents(transcript)
@@ -40,13 +40,13 @@ def create_vector_db_from_youtube_url(video_url:str) -> FAISS:
 
 @newrelic.agent.background_task()
 def get_response_from_query(db, query, k=4): #k represents the # of Documents to send to stay within the token context window
-    #gpt-3.5-turbo-instruct has a context window of 4096 tokens
+    #gpt-3.5-turbo has a context window of 4096 tokens
 
     docs = db.similarity_search(query, k=k) #this will only search the documents relevant to the user's query
     docs_page_content = " ".join([d.page_content for d in docs]) #combines the 4 docs into a single doc
 
     #Work with the LLM - 
-    llm = OpenAI(model="gpt-3.5-turbo-instruct") 
+    llm = ChatOpenAI(model="gpt-3.5-turbo") 
     #Work with the Prompt
     prompt = PromptTemplate(
         input_variables=["question", "docs"], #docs is the similarity search
@@ -69,6 +69,6 @@ def get_response_from_query(db, query, k=4): #k represents the # of Documents to
     chain = LLMChain(llm=llm, prompt=prompt)
 
     # Run the Langchain module with the New Relic Callback - STEP 3
-    response = chain.run(question=query, docs=docs_page_content)
+    response = chain.run(question=query, docs=docs_page_content, callbacks=[new_relic_monitor])
     response = response.replace("\n", "") #formatting
     return response, docs 
